7e9d3ca20ea12575a622fa1cd350454fcc39eb0b
Fatima Braz <fatima.braz@ibm.com>
2021-06-29 12:56:18 -0300

    t4

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var express = require('express');
 var app = express();
 
 app.post('/', function(req,res) {
-       res.send( "oie");
+   
+    res.send('xxx');
+
+});
 
 app.listen(process.env.PORT||3000, function () {
     console.log('Executando na porta 3000');
