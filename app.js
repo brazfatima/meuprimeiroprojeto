857d839c83dbcf8a1482303b40d226e52008b33f
Fatima Braz <fatima.braz@ibm.com>
2021-06-29 13:44:52 -0300

    t

--- app.js
+++ app.js
@@ -5,18 +5,7 @@ var app = express();
 
 app.post('/', function(req,res) {
    
-    res.send({
-        "blocks": [
-            {
-                "type": "header",
-                "text": {
-                    "type": "plain_text",
-                    "text": "Accrual Status",
-                    "emoji": true
-                }
-            }
-        ]
-    });
+    res.send("teste");
 
 app.listen(process.env.PORT||3000, function () {
     console.log('Executando na porta 3000');
