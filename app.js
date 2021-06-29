522f454d444a3c047da4892ebcc56af3e4bd1e40
Fatima Braz <fatima.braz@ibm.com>
2021-06-29 15:15:19 -0300

    t

--- app.js
+++ app.js
@@ -6,20 +6,60 @@ var app = express();
 app.post('/', function(req,res) {
    
     res.send(
-    {
-        "blocks": [
-            {
-                "type": "header",
-                "text": {
-                    "type": "plain_text",
-                    "text": "Accrual Status",
-                    "emoji": true
+        {
+            "blocks": [
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Accrual Status",
+                        "emoji": true
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Pick an item from the dropdown list"
+                    },
+                    "accessory": {
+                        "type": "static_select",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select an item",
+                            "emoji": true
+                        },
+                        "options": [
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "*this is plain_text text*",
+                                    "emoji": true
+                                },
+                                "value": "value-0"
+                            },
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "*this is plain_text text*",
+                                    "emoji": true
+                                },
+                                "value": "value-1"
+                            },
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "*this is plain_text text*",
+                                    "emoji": true
+                                },
+                                "value": "value-2"
+                            }
+                        ],
+                        "action_id": "static_select-action"
+                    }
                 }
-            }
-        ]
-    }
-    
-    
+            ]
+        }
     );
 
 });
